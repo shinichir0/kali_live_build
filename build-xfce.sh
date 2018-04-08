e2f16fb222222a851da4748f4009eeb0635921aa
shinihchir0 <18505311+shinichir0@users.noreply.github.com>
2018-04-08 16:40:27 +0900

    update

--- build-xfce.sh
+++ build-xfce.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "install requirement============================================"
 cd /root
 apt-get update
 apt-get upgrade -y
@@ -7,10 +8,13 @@ apt-get dist-upgrade -y
 apt-get autoremove -y
 apt-get autoclean -y
 apt-get update
-apt-get install -y git live-build cdebootstrap
+apt-get install -y curl git live-build
+
+echo "cloning repository============================================="
 git clone git://git.kali.org/live-build-config.git build
 cd build
 
+echo "copy config files=============================================="
 cp /root/kali_live_build/cfg/isolinux.cfg /root/build/kali-config/common/includes.binary/isolinux/
 cp /root/kali_live_build/cfg/menu.cfg /root/build/kali-config/common/includes.binary/isolinux/
 cp /root/kali_live_build/cfg/live.cfg /root/build/kali-config/common/includes.binary/isolinux/
@@ -18,4 +22,5 @@ cp /root/kali_live_build/cfg/live.cfg /root/build/kali-config/common/includes.bi
 cp /root/kali_live_build/cfg/0031-root-password /root/build/kali-config/common/includes.chroot/usr/lib/live/config/
 cp /root/kali_live_build/cfg/xfce/kali.list.chroot /root/build/kali-config/variant-xfce/package-lists/
 
+echo "start build===================================================="
 ./build.sh --distribution kali-rolling --variant xfce --verbose
