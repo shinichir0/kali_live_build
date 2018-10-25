a9b2c0a2da067c318f2a39d33dd8285236fbb21f
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-10-26 07:20:25 +0900

    update

--- test.sh
+++
@@ -1,6 +0,0 @@
-
-git clone git://git.kali.org/live-build-config.git build
-cd build
-CHANGE_NUM=$(cat build.sh | grep -n -A 1 "debootstrap" | grep exit | awk -F - '{print $1}')
-echo $CHANGE_NUM
-sed -e "${CHANGE_NUM} s/exit/# exit/g" build.sh | sudo tee build.sh
